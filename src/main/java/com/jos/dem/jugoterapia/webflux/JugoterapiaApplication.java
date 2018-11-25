e95ac07b62f693070b83640b69a44219d5fd8d89
josdem <joseluis.delacruz@gmail.com>
2018-11-25 09:01:38 -0500

    Adding category class

--- src/main/java/com/jos/dem/jugoterapia/webflux/JugoterapiaApplication.java
+++ src/main/java/com/jos/dem/jugoterapia/webflux/JugoterapiaApplication.java
@@ -1,12 +1,13 @@
-package com.example.jugoterapiawebflux;
+package com.jos.dem.jugoterapia.webflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class JugoterapiaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+    SpringApplication.run(JugoterapiaApplication.class, args);
 	}
+
 }
