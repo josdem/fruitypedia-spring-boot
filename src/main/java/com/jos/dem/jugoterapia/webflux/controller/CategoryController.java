2b6b724ea1a591d30ba0ae1027d56104ab8f7769
josdem <joseluis.delacruz@gmail.com>
2018-11-25 09:47:28 -0500

    Adding category controller

---
+++ src/main/java/com/jos/dem/jugoterapia/webflux/controller/CategoryController.java
@@ -0,0 +1,37 @@
+/*
+  Copyright 2018 José Luis De la Cruz Morales <joseluis.delacruz@gmail.com>
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package com.jos.dem.jugoterapia.webflux.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.jos.dem.jugoterapia.webflux.model.Category;
+import com.jos.dem.jugoterapia.webflux.service.CategoryService;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+  @Autowired
+  private CategoryService categoryService;
+
+  @GetMapping("/")
+  public Flux<Category> getCategories(){
+    log.info("Listing categories");
+    categoryService.findAll();
+  }
+
+}
