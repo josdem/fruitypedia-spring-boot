ee8f28195cfef2fd6335a0939c721b04c48eb003
josdem <joseluis.delacruz@gmail.com>
2019-01-02 13:20:22 -0500

    #6 Adding getters to the category model

--- src/main/java/com/jos/dem/jugoterapia/webflux/model/Category.java
+++ src/main/java/com/jos/dem/jugoterapia/webflux/model/Category.java
@@ -18,18 +18,19 @@ import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-
-@Data
 @Document
-@NoArgsConstructor
-@AllArgsConstructor
 public class Category {
 
   @Id
   private Integer id;
   private String name;
+
+  public Integer getId(){
+    return id;
+  }
+
+  public String getName(){
+    return name;
+  }
 
 }
